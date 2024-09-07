6680b2cd8a57a81efcd1632eda5b188c058ba292
Ashik <engineerashikur@gmail.com>
2024-09-07 11:11:55 +0600

    common a get element value function added

--- geometry.js
+++ geometry.js
@@ -6,12 +6,13 @@ document.getElementById('triangle-b').addEventListener('click', function triangl
     //console.log('Button xxx clicked');
     const triInOneValue = triInOne.value;
     const triInOneValueParse = parseFloat(triInOneValue);
+    triInOne.value="";
 
     const triInTwo = document.getElementById('triangle-y');
     //console.log('Button yyy clicked');
     const triInTwoValue = triInTwo.value;
     const triInTwoValueParse = parseFloat(triInTwoValue);
-
+    triInTwo.value="";
     const area = ( 0.5 * triInOneValueParse * triInTwoValueParse );
     console.log(area);
 
@@ -20,5 +21,18 @@ document.getElementById('triangle-b').addEventListener('click', function triangl
     const triTextFloat = parseFloat(triTextValue); 
 
     triText.innerText = area;
-    
+})
+
+//Get Element Code Here
+function getElementValues(elements){
+const getElementId = document.getElementById(elements);
+const getElementValue = getElementId.value;
+const getElementFloat = parseFloat(getElementValue);
+getElementId.value="";
+return getElementFloat;
+}
+
+document.getElementById('circle-b').addEventListener('click', function circle(){
+    const circlebutt = getElementValues('circleX');
+    console.log(circlebutt);
 })
