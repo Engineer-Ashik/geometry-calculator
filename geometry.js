0f8650f953e29f611812d994f65e851969b13263
Ashik <engineerashikur@gmail.com>
2024-09-11 08:35:31 +0600

    Completed the Calculation Area Section

--- geometry.js
+++ geometry.js
@@ -1,36 +1,5 @@
 console.log("added successfully");
 
-//Triangle button
-document.getElementById('triangle-btn').addEventListener('click', function triangle() {
-    //console.log('Button clicked');
-    const triInOne = document.getElementById("triangle-x");
-    //console.log('Button xxx clicked');
-    const triInOneValue = triInOne.value;
-    const triInOneValueParse = parseFloat(triInOneValue);
-    triInOne.value = "";
-
-    const triInTwo = document.getElementById('triangle-y');
-    //console.log('Button yyy clicked');
-    const triInTwoValue = triInTwo.value;
-    const triInTwoValueParse = parseFloat(triInTwoValue);
-    triInTwo.value = "";
-
-    if(triInOneValue >= 0 && triInTwoValue >= 0){
-        const area = (0.5 * triInOneValueParse * triInTwoValueParse);
-        console.log(area);    
-    }
-    else {
-        alert('Please Enter A Valid Input');
-    }
-
-
-    const triText = document.getElementById('tri-text');
-    const triTextValue = triText.innerText;
-    const triTextFloat = parseFloat(triTextValue);
-
-    triText.innerText = area;
-})
-
 //Get Element reusable Code Here
 function getElementValues(elementId) {
     const getElementId = document.getElementById(elementId);
@@ -49,7 +18,7 @@ function setElementValues(elementId, values) {
 }
 
 //Extra Area Calculation list 
-function addToCalculationEntry(areaType, area ){
+function addToCalculationEntry(areaType, area){
     console.log('Added calculationEntry Function now....');
     console.log(areaType + ' '+ area);
     const calculationEntry = document.getElementById('calculation-entry'); // id of area section div
@@ -58,8 +27,38 @@ function addToCalculationEntry(areaType, area ){
     const paragraph = document.createElement('p');
     paragraph.innerHTML = counting+1 + ' ' + areaType + ' ' + area + 'cm<sup>2</sup>'; // `${areaType} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-success">Meter</button>`
     calculationEntry.appendChild(paragraph);
-
 }
+
+//Triangle button
+document.getElementById('triangle-btn').addEventListener('click', function triangle() {
+    //console.log('Button clicked');
+    const triInOne = document.getElementById("triangle-x");
+    //console.log('Button xxx clicked');
+    const triInOneValue = triInOne.value;
+    const triInOneValueParse = parseFloat(triInOneValue);
+    triInOne.value = "";
+
+    const triInTwo = document.getElementById('triangle-y');
+    //console.log('Button yyy clicked');
+    const triInTwoValue = triInTwo.value;
+    const triInTwoValueParse = parseFloat(triInTwoValue);
+    triInTwo.value = "";
+
+    if(triInOneValue >= 0 && triInTwoValue >= 0){
+        const area = (0.5 * triInOneValueParse * triInTwoValueParse);
+        const triText = document.getElementById('tri-text');
+        const triTextValue = triText.innerText;
+        const triTextFloat = parseFloat(triTextValue);
+        triText.innerText = area;
+        addToCalculationEntry('TRIANGLE' ,area);
+        //console.log(area);    
+    }
+    else {
+        alert('Please Enter A Valid Input');
+    }
+
+
+})
 
 //circle button
 document.getElementById('circle-btn').addEventListener('click',function circle(){
@@ -79,7 +78,8 @@ document.getElementById('rectangle-btn').addEventListener('click',function recta
     const rectangleInputL = getElementValues('rectangle-l');
     if( rectangleInputW >= 0 && rectangleInputL >= 0){
     const area = (rectangleInputW * rectangleInputL);
-    const rectangleText = setElementValues('rectangle-text', area);}
+    const rectangleText = setElementValues('rectangle-text', area);
+    addToCalculationEntry('REACANGLE OR SQUARE', area);}
     else{
         alert('Please Enter A Valid Input');
     }
@@ -91,7 +91,8 @@ document.getElementById('parallelogram-btn').addEventListener('click',function p
     const parallelogramInputH = getElementValues('parallelogram-hight');
     if( parallelogramInputB >= 0 && parallelogramInputH >= 0 ){
     const area = ( parallelogramInputB * parallelogramInputH );
-    const parallelogramText = setElementValues('parallelogram-text', area);}
+    const parallelogramText = setElementValues('parallelogram-text', area);
+    addToCalculationEntry('PARALLELOGRAM' ,area);}
     else{
         alert('Please Enter A Valid Input');
     }
@@ -103,7 +104,8 @@ document.getElementById('rhombus-btn').addEventListener('click',function rhombus
     const rhombusInputQ = getElementValues('rhombus-q');
     if( rhombusInputP >= 0 && rhombusInputQ >= 0 ){
     const area = ( rhombusInputP * rhombusInputQ );
-    const rhombusText = setElementValues('rhombus-text', area);}
+    const rhombusText = setElementValues('rhombus-text', area);
+    addToCalculationEntry('RHOMBUS' ,area);}
     else{
         alert('Please Enter A Valid Input');
     }
@@ -115,7 +117,8 @@ document.getElementById('pentagon-btn').addEventListener('click',function pentag
     const pentagonInputA = getElementValues('pentagon-a');
     if( pentagonInputA >= 0 && pentagonInputP >= 0){
     const area = ( 0.5 * pentagonInputP * pentagonInputA );
-    const pentagonText = setElementValues('pentagon-text', area);}
+    const pentagonText = setElementValues('pentagon-text', area);
+    addToCalculationEntry('PENTAGON' ,area);}
     else{
         alert('Please Enter A Valid Input');
     }
@@ -127,7 +130,8 @@ document.getElementById('ellipse-btn').addEventListener('click',function ellipse
     const ellipseInputB = getElementValues('ellipse-b');
     if( ellipseInputA >= 0 && ellipseInputB >= 0 ){
     const area = ( 3.1416 * ellipseInputA * ellipseInputB );
-    const ellipseText = setElementValues('ellipse-text', area);}
+    const ellipseText = setElementValues('ellipse-text', area);
+    addToCalculationEntry('ELLIPSE' ,area);}
     else{
         alert('Please Enter A Valid Input');
     }
@@ -138,7 +142,8 @@ document.getElementById('hexagon-btn').addEventListener('click',function hexagon
     const hexagonInput = getElementValues('hexagon-a');
     if( hexagonInput >= 0 ){
     const area = ( 0.5 * 3 * 1.7321 * hexagonInput * hexagonInput );
-    const hexagonText = setElementValues('hexagon-text', area);}
+    const hexagonText = setElementValues('hexagon-text', area);
+    addToCalculationEntry('HEXAGON' ,area);}
     else{
         alert('Please Enter A Valid Input');
     }
@@ -151,7 +156,8 @@ document.getElementById('trapezium-btn').addEventListener('click',function trape
     const trapeziumInputH = getElementValues('trapezium-h');
     if( trapeziumInputA >= 0 && trapeziumInputB >= 0 && trapeziumInputH >=0 ){
     const area = ( 0.5 * trapeziumInputH * (trapeziumInputA + trapeziumInputB));
-    const trapeziumText = setElementValues('trapezium-text', area);}
+    const trapeziumText = setElementValues('trapezium-text', area);
+    addToCalculationEntry('TRAPEZIUM' ,area);}
     else{
         alert('Please Enter A Valid Input');
     }
